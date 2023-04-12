5585d4aeeba9ed09a4f3a6e9c03533d428c3f087
pablocastro <pablocastro079@gmail.com>
2023-04-12 09:49:46 -0500

    Added error handling if csv does not exist

--- file_read.py
+++ file_read.py
@@ -3,6 +3,10 @@ import csv
 
 # Read csv file as dictionary
 def readEmployeesCSV():
-    with open('employees.csv', 'r') as file:
-        employees = [line for line in csv.DictReader(file)]
-    return employees
+    try:
+        with open('employees.csv', 'r') as file:
+            employees = [line for line in csv.DictReader(file)]
+        return employees
+    except FileNotFoundError:
+        # If file not found, return empty dictionary
+        return {}
