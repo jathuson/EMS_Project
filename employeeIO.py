bdc1b78edfc76bb55f11f7a397b8f0c5d96ab7bb
Nikkiskybear <nikkiskybear@gmail.com>
2023-04-12 12:21:53 -0400

    got rid of the openfile func

--- employeeIO.py
+++ employeeIO.py
@@ -2,7 +2,17 @@ import csv
 import employee
 
 
-def openEmployeeFile():
+
+    
+
+  
+
+def closeEmployeeFile(csvFile):
+
+    csvFile.close()
+
+# Read csv file as dictionary
+def readEmployeesFile():
     try:
         csvFile = open("employees.csv", "r+")
     except FileNotFoundError:
@@ -11,23 +21,19 @@ def openEmployeeFile():
                       'startDate', 'salary', 'department', 'empID']
         write = csv.DictWriter(csvFile, fieldnames=fieldnames)
         write.writeheader()
-
-    return csvFile
-
-
-def closeEmployeeFile(csvFile):
-
-    csvFile.close()
-
-# Read csv file as dictionary
-def readEmployeesFile():
-    file = openEmployeeFile()
     employees = [line for line in csv.DictReader(file)]
     return employees
 
 # Write new employee to csv file
 def writeNewEmployee(employee):
-    file = openEmployeeFile()
+    try:
+        file = open("employees.csv", "a")
+    except FileNotFoundError:
+        file = open("employees.csv", "w")
+        fieldnames = ['firstName', 'lastName',
+                      'startDate', 'salary', 'department', 'empID']
+        write = csv.DictWriter(file, fieldnames=fieldnames)
+        write.writeheader()
     fieldnames = ['firstname', 'lastname',
                   'startDate', 'salary', 'department', 'empID']
     write = csv.DictWriter(file, fieldnames=fieldnames)
