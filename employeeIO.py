28fa58bbde1160e6bc00fee80e072ef90cd5ea4c
pablocastro <pablocastro079@gmail.com>
2023-04-13 10:27:50 -0500

    removed duplicate removeEmployee()

--- employeeIO.py
+++ employeeIO.py
@@ -66,13 +66,15 @@ def updateEmployee(employee):
             writer.writerow(row)
     shutil.move(tempFile.name, "employees.csv")
 
+
 def removeEmployee(employee):
-    
+
     tempFile = NamedTemporaryFile(mode="w", delete=False)
- 
-    fieldnames = ['firstName', 'lastName','startDate', 'salary', 'department', 'empId']
-    
-    filename ="employees.csv"
+
+    fieldnames = ['firstName', 'lastName',
+                  'startDate', 'salary', 'department', 'empId']
+
+    filename = "employees.csv"
 
     with open(filename, 'r+') as file, tempFile:
         writer = csv.DictWriter(tempFile, fieldnames=fieldnames)
@@ -80,31 +82,10 @@ def removeEmployee(employee):
         employee = employee.toDict()
         for row in reader:
             print(row)
-            if row['empId'] != str(employee['empId']):                  
-                row = {'firstName': row['firstName'], 'lastName': row['lastName'], 'startDate': row['startDate'], 'salary': row['salary'], 'department': row['department'], 'empId': row['empId']}
+            if row['empId'] != str(employee['empId']):
+
+                row = {'firstName': row['firstName'], 'lastName': row['lastName'], 'startDate': row['startDate'],
+                       'salary': row['salary'], 'department': row['department'], 'empId': row['empId']}
                 writer.writerow(row)
     shutil.move(tempFile.name, "employees.csv")
-
-def removeEmployee(employee):
-    tempFile = NamedTemporaryFile(mode="w", suffix=".py", prefix="employees")
- 
-    fieldnames = ['firstName', 'lastName','startDate', 'salary', 'department', 'empId']
-    try:
-        file = open("employees.csv", "r")
-    except FileNotFoundError:
-        file = open("employees.csv", "w")
-        fieldnames = ['firstName', 'lastName',
-                      'startDate', 'salary', 'department', 'empId']
-        write = csv.DictWriter(file, fieldnames=fieldnames)
-        write.writeheader()
-    writer = csv.DictWriter(tempFile, fieldnames=fieldnames)
-    reader = csv.DictReader(file, fieldnames=fieldnames)
-    employee = employee.toDict()
-    for row in reader:
-        if row['empId'] != str(employee['empId']):
-            row = {'firstName': row['firstName'], 'lastName': row['lastName'], 'startDate': row['startDate'], 'salary': row['salary'], 'department': row['department'], 'empId': row['empId']}
-            writer.writerow(row)
-    shutil.move(tempFile.name, "employees.csv")
-    tempFile.close()
-    file.close()
 
